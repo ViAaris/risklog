92e05100e2ed8c18be7dd2c6aa7c353e5ca206fa
Viktoria Aaris <viktoria_aaris@hotmail.com>
2022-09-05 19:27:07 +0200

    fix login and add validation

--- src/main/java/com/bygst/risikolog/exceptions/InvalidDataException.java
+++
@@ -1,7 +0,0 @@
-package com.bygst.risikolog.exceptions;
-
-public class InvalidDataException extends RuntimeException{
-    public InvalidDataException(String message) {
-        super(message);
-    }
-}
